b9e1f977c9ddd7af098f44b6de409335cd3191e6
David Makhija <Albein81000@gmail.com>
2023-06-10 11:10:10 +0530

    Optimized after reading the editorial further(Accepted)

--- 4 June - 11 June/Leetcode-1354.cpp
+++ 4 June - 11 June/Leetcode-1354.cpp
@@ -3,7 +3,7 @@ public:
     bool isPossible(vector<int>& target) {
         if(target.size()==1) return (target[0]==1) ? true : false;
         priority_queue<int> pq;
-        int sum = 0;
+        long long sum = 0;
         for(int x:target) {
             sum += x;
             pq.push(x);
@@ -11,8 +11,10 @@ public:
         while(pq.top()>1) {
             int temp = pq.top();
             pq.pop();
-            int remainingSum = sum - temp;
-            temp = temp - remainingSum;
+            long long remainingSum = sum - temp;
+            if(remainingSum==1) return true;
+            if(temp==remainingSum or temp<remainingSum) return false;
+            temp = temp%remainingSum;
             if(temp<1) return false;
             pq.push(temp);
             sum = temp + remainingSum;
