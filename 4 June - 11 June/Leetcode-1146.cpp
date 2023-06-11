fba850c7cfc98db66e439794d249e96385edc59f
David Makhija <Albein81000@gmail.com>
2023-06-11 19:45:59 +0530

    Copied Solution :(  - (You have forgotten about the upper_bound function)

--- 4 June - 11 June/Leetcode-1146.cpp
+++ 4 June - 11 June/Leetcode-1146.cpp
@@ -1,39 +1,25 @@
 class SnapshotArray {
-    int snap_id = 0;
-    unordered_map<int, unordered_map<int, int>> snapshots;
-    vector<int> changed;
-    int n;
 public:
+    int snapId;
+    vector<vector<pair<int, int>>> historyRecords;
     SnapshotArray(int length) {
-        n = length;
-        for(int i=0; i<length; i++) {
-            changed.push_back(-1);
+        snapId = 0;
+        historyRecords.resize(length);
+        for (int i = 0; i < length; ++i) {
+            historyRecords[i].push_back(make_pair(0, 0));
         }
     }
     
     void set(int index, int val) {
-        changed[index] = val;
+        historyRecords[index].push_back(make_pair(snapId, val));
     }
     
     int snap() {
-        unordered_map<int, int> temp;
-        for(int i=0; i<n; i++) {
-            if(changed[i]!=-1) {
-                temp[i] = changed[i];
-                changed[i] = -1;
-            }
-        }
-        snapshots[this->snap_id] = temp;
-        return this->snap_id++;
+        return snapId++;
     }
     
     int get(int index, int snap_id) {
-        int lo=0, hi=snap_id+1;
-        while(lo<hi) {
-            int mid = lo + ((hi-lo)/2);
-            if(snapshots[mid].find(index)!=snapshots[mid].end()) lo = mid+1;
-            else hi = mid;
-        }
-        return (lo>0) ? snapshots[lo-1][index] : 0;
+        auto it = upper_bound(historyRecords[index].begin(), historyRecords[index].end(), make_pair(snap_id, INT_MAX));
+        return prev(it)->second;
     }
 };
