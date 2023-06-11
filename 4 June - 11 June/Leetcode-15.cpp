04bad9943db583ec355f9b1f468aaa8296d38882
David Makhija <Albein81000@gmail.com>
2023-06-11 16:53:40 +0530

    Fixed something again, had to update lo & hi regularly Still it says TLE

--- 4 June - 11 June/Leetcode-15.cpp
+++ 4 June - 11 June/Leetcode-15.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
-        int n = nums.size(), prev=INT_MAX, prevlo=INT_MAX, prevhi=INT_MIN;
+        int n = nums.size(), prev=INT_MAX;
         sort(nums.begin(), nums.end());
         for(int i=0; i<n; i++) {
             if(nums[i]==prev) continue;
             if(nums[i]>0) break;
+            cout<<nums[i]<<endl;
             int lo=i+1, hi=n-1;
+            int prevlo=INT_MAX, prevhi=INT_MIN;
             while(lo<hi) {
+                cout<<" "<<lo<<", "<<hi<<";";
                 if((long long)(nums[lo]+nums[hi]+nums[i])==0) {
                     ans.push_back({nums[i], nums[lo], nums[hi]});
                     prevlo = nums[lo], prevhi = nums[hi];
@@ -25,6 +28,7 @@ public:
                 while(lo<hi and nums[lo]==prevlo) lo++;
                 while(lo<hi and nums[hi]==prevhi) hi--;
             }
+            cout<<endl;
             prev = nums[i];
         }
         return ans;
