f1c4abae5ace4651d2539140c22ff67851dc62fd
David Makhija <Albein81000@gmail.com>
2023-06-11 17:01:50 +0530

    Copied Solution (Didn't understand how it's different from my approach)

--- 4 June - 11 June/Leetcode-15.cpp
+++ 4 June - 11 June/Leetcode-15.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        int n = nums.size(), prev=INT_MAX;
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<n; i++) {
-            if(nums[i]==prev) continue;
-            if(nums[i]>0) break;
-            cout<<nums[i]<<endl;
-            int lo=i+1, hi=n-1;
-            int prevlo=INT_MAX, prevhi=INT_MIN;
-            while(lo<hi) {
-                cout<<" "<<lo<<", "<<hi<<";";
-                if((long long)(nums[lo]+nums[hi]+nums[i])==0) {
-                    ans.push_back({nums[i], nums[lo], nums[hi]});
-                    prevlo = nums[lo], prevhi = nums[hi];
-                    lo++, hi--;
-                }
-                else if((long long)(nums[lo]+nums[hi]+nums[i])<0) {
-                    prevlo = nums[lo];
-                    lo++;
-                }
-                else if((long long)(nums[lo]+nums[hi]+nums[i])>0) {
-                    prevhi = nums[hi];
-                    hi--;
-                }
-                while(lo<hi and nums[lo]==prevlo) lo++;
-                while(lo<hi and nums[hi]==prevhi) hi--;
+        sort(begin(nums), end(nums));
+        vector<vector<int>> res;
+        for (int i = 0; i < nums.size() && nums[i] <= 0; ++i)
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSumII(nums, i, res);
             }
-            cout<<endl;
-            prev = nums[i];
+        return res;
+    }
+    void twoSumII(vector<int>& nums, int i, vector<vector<int>> &res) {
+        int lo = i + 1, hi = nums.size() - 1;
+        while (lo < hi) {
+            int sum = nums[i] + nums[lo] + nums[hi];
+            if (sum < 0) {
+                ++lo;
+            } else if (sum > 0) {
+                --hi;
+            } else {
+                res.push_back({ nums[i], nums[lo++], nums[hi--] });
+                while (lo < hi && nums[lo] == nums[lo - 1])
+                    ++lo;
+            }
         }
-        return ans;
     }
 };
