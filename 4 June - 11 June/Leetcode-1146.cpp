36bfc477b5f839961f4fd3270489ff569bdd4199
David Makhija <Albein81000@gmail.com>
2023-06-11 18:40:11 +0530

    Coded on my own reading the editorial just a bit(Wrong Answer)

--- 4 June - 11 June/Leetcode-1146.cpp
+++ 4 June - 11 June/Leetcode-1146.cpp
@@ -1,24 +1,39 @@
 class SnapshotArray {
     vector<int> arr;
-    vector<vector<int>> snapshots;
+    int snap_id = 0;
+    unordered_map<int, unordered_map<int, int>> snapshots;
+    vector<int> changed;
+    int n;
 public:
     SnapshotArray(int length) {
+        n = length;
         for(int i=0; i<length; i++) {
             arr.push_back(0);
+            changed.push_back(-1);
         }
     }
     
     void set(int index, int val) {
+        changed[index] = val;
         arr[index] = val;
     }
     
     int snap() {
-        vector<int> temp = arr;
-        snapshots.push_back(temp);
-        return snapshots.size()-1;
+        unordered_map<int, int> temp;
+        for(int i=0; i<n; i++) {
+            if(changed[i]!=-1) {
+                temp[i] = changed[i];
+                changed[i] = -1;
+            }
+        }
+        snapshots[this->snap_id] = temp;
+        return this->snap_id++;
     }
     
     int get(int index, int snap_id) {
-        return snapshots[snap_id][index];
+        for(auto itr=snapshots.begin(); itr!=snapshots.end(); itr++) {
+            if(itr->first<=snap_id and itr->second.find(index)!=itr->second.end()) return itr->second[index];
+        }
+        return 0;
     }
 };
