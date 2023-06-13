e0ee39f28aa62de05aa31c022fa22fdad1d6afbb
David Makhija <Albein81000@gmail.com>
2023-06-13 09:34:46 +0530

    Solution after taking some hints(Wrong Answer)

--- 12 June - 18 June/Leetcode-444.cpp
+++ 12 June - 18 June/Leetcode-444.cpp
@@ -1,27 +1,61 @@
-class Node {
-public:
-    int val;
-    Node* next;
-    Node(int val) {
-        this->val = val;
-        next = NULL;
-    }
-}
-bool comp(vector<int> a, vector<int> b) {
-    return a.size()<=b.size();
-}
 class Solution {
+    int elementsTraversed = 0;
+    bool isLinear = false;
+    void topoSort(int idx, vector<int>* adj, vector<bool>& vis, stack<int>& st) {
+        if(vis[idx]) return;
+        vis[idx] = true;
+        for(int x:adj[idx]) {
+            topoSort(x, adj, vis, st);
+        }
+        st.push(idx);
+    }
+    bool cycleCheck(int idx, vector<int>* adj, vector<bool>& vis, vector<bool>& dfsvis, int n) {
+        if(dfsvis[idx]) return true;
+        if(vis[idx]) return false;
+        elementsTraversed++;
+        vis[idx] = true;
+        dfsvis[idx] = true;
+        for(int x:adj[idx]) {
+            if(cycleCheck(x, adj, vis, dfsvis, n)) return true;
+        }
+        if(elementsTraversed==n) isLinear = true;
+        elementsTraversed--;
+        dfsvis[idx] = false;
+        return false;
+    }
 public:
     bool sequenceReconstruction(vector<int>& nums, vector<vector<int>>& sequences) {
         int n = nums.size();
-        vector<Node*> nodes(n+1, NULL);
-        sort(sequences.begin(), sequences.end(), comp);
+        vector<int> adj[n+1];
         int m = sequences.size();
         for(int i=0; i<m; i++) {
             int l = sequences[i].size();
+            int prev = 0;
             for(int j=0; j<l; j++) {
-                
+                if(prev>0) adj[prev].push_back(sequences[i][j]);
+                prev = sequences[i][j];
             }
         }
+        for(int i=1; i<=n; i++) {
+            cout<<i<<" -> ";
+            for(int x:adj[i]) cout<<x<<" ";
+            cout<<endl;
+        }
+        stack<int> st;
+        vector<bool> vis(n+1, false);
+        vector<bool> dfsvis(n+1, false);
+        for(int i=1; i<=n; i++) {
+            if(!vis[i]) topoSort(i, adj, vis, st);
+        }
+        for(int i=1; i<=n; i++) {
+            vis[i] = false;
+        }
+        if(cycleCheck(st.top(), adj, vis, dfsvis, n)) return false;
+        for(int i=1; i<=n; i++) {
+            if(!vis[i]) {
+                return false;
+            }
+        }
+        return isLinear;
     }
 };
