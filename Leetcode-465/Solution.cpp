e60868bdd8afeb67c9ea69fd2b39317c7de5259c
David Makhija <Albein81000@gmail.com>
2023-07-01 16:22:24 +0530

    Clarity Questions Asked ✅

---
+++ Leetcode-465/Solution.cpp
@@ -0,0 +1,10 @@
+// Clarity Questions :
+// What's the limit on the number of transactions
+// What's the limit on amounti?
+
+class Solution {
+public:
+    int minTransfers(vector<vector<int>>& transactions) {
+        
+    }
+};
